d3c7f23efa3b0ec54c944d38e72ae6ca1c0ff309
Computer <Computer>
2017-07-17 09:39:44 +0200

    test if both url and names are defined and not empty

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,12 +31,29 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('url are defined',function(){
+            allFeeds.forEach(function(feed) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).not.toBe(0);
 
+            });
 
+         });
+       
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+          it('names are defined', function() {
+                    allFeeds.forEach(function(feed) {
+                        expect(feed.name).toBeDefined();
+                        expect(feed.name.length).not.toBe(0);
+
+                    });
+                            
+                });
+         
+
     });
 
 
